4d7bde671f0fc5f12e21a16b585833678f78da5e
mgreco2004 <marcogreco2004@outlook.com>
2025-03-11 20:33:21 -0400

    created vector of processes

--- a2.cpp
+++ a2.cpp
@@ -1,8 +1,77 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <chrono>
+#include <string>
 
 using namespace std;
 
+class Process {
+public:
+	string userID;
+	int readyTime;
+	int serviceTime;
+};
+
 int main() {
+
+	ifstream inputFile("input.txt");
+
+	if (!inputFile) {
+		cerr << "Error opening input file";
+		return 1;
+	}
+
+	int quantum;
+	vector<Process> Processes;
+	string value;
+	char valChar;
+	string amountProcessesString = "0";
+	int amountProcesses = 0;
+	string readyTimeString;
+	int readyTime;
+	string serviceTimeString;
+	int serviceTime;
+
+	inputFile >> quantum;
+
+	cout << "Quantum: " << quantum << "\n";
+
+	char userID = NULL;
+
+	while (inputFile >> value) {
+			valChar = value[0];
+			if (static_cast<int>(valChar) >= 65 && static_cast<int>(valChar) <= 90) {
+				userID = valChar;
+				inputFile >> amountProcessesString;
+				amountProcesses = stoi(amountProcessesString);
+				for (int i = 0; i < amountProcesses; i++) {
+					for (int j = 0; j <= 2; j++) {
+						if (j == 0) {
+							inputFile >> readyTimeString;
+							readyTime = stoi(readyTimeString);
+						}
+						else if (j == 1) {
+							inputFile >> serviceTimeString;
+							serviceTime = stoi(serviceTimeString);
+						}
+						else {
+							Process process;
+							process.userID = userID;
+							process.readyTime = readyTime;
+							process.serviceTime = serviceTime;
+							Processes.push_back(process);
+						}
+					}
+				}
+			}
+		
+	}
+
+	for (int i = 0; i < Processes.size(); i++) {
+		cout << "User ID: " << Processes[i].userID << " | " << "Ready Time: " << Processes[i].readyTime << " | " << "Service Time: " << Processes[i].serviceTime << "\n";
+	}
+
+
 
 }
